fd037c36269da6868acb5f97a714a88bd18564c5
League Student <league@iMac-23.attlocal.net>
2019-10-02 18:28:58 -0700

    templates
    templates

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,17 +3,35 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	//Object[] array = new Object[10];
+	Object[] array = (Object[]) new Object[10];
+
 	public ArrayList() {
+
 	}
 	
-	public T get(int loc) throws IndexOutOfBoundsException {
-		
+	public Object get(int loc) throws IndexOutOfBoundsException {
+		for (int i = 0; i < array.length; i++) {
+			if(i == loc) {
+				return array[i];
+			}
+		}
 		return null;
 	}
 	
 	public void add(T val) {
-		
+		Object[] newArray = new Object[array.length + 1];
+		for (int i = 0; i < array.length; i++) {
+			if(i == 10) {
+				newArray[i] = val;
+			}
+			else {
+			newArray[i] = array[i];
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			array[i] = newArray[i];
+		}
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
